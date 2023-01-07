a41dd1461905314fcdbfbdcd654ae95e4338c7aa
Никита <manticore1305@gmail.com>
2023-01-07 19:14:13 +0300

    start screen animations

--- start_screen.py
+++ start_screen.py
@@ -5,6 +5,7 @@ import pygame as pg
 from constants import FPS, SIZE, TETRIS_LOGO_SIZE, START_SCREEN_BUTTONS, BUTTON_COLOR, BUTTON_TARGETED_COLOR, \
     DECO_TEXT_COLOR
 from draw_button import draw_button
+from tetris_logo_animation import tetris_logo_animation
 
 
 def terminate():  # Прерывание игры (через pg.QUIT или кнопкой "Выйти")
@@ -17,9 +18,9 @@ def start_screen():  # Стартовое окно
     screen = pg.display.set_mode(SIZE)
     clock = pg.time.Clock()
 
-    tetris_logo = pg.transform.scale(pg.image.load('data/tetris_logo.png'), TETRIS_LOGO_SIZE)
-    # Загружаем логотип "Тетриса"
-    screen.blit(tetris_logo, (5, 75))  # Отображаем его на экране
+    cycle_iterations = 0  # Подсчёт итераций цикла (для работы анимаций)
+    logo_frame = 1  # Номер кадра логотипа
+    deco_frame = 0
 
     font = pg.font.Font(None, 40)  # Шрифт текста на кнопках
 
@@ -27,9 +28,7 @@ def start_screen():  # Стартовое окно
         draw_button(screen, button, BUTTON_COLOR, font)
 
     deco_font = pg.font.Font(None, 50)  # Декоративный текст сверху и снизу экрана
-    deco_text = deco_font.render('- × - × - × - × - × - × - × - × - × -', True, DECO_TEXT_COLOR)
-    screen.blit(deco_text, (12, 10))
-    screen.blit(deco_text, (12, 580))
+
 
     while True:
         for event in pg.event.get():
@@ -57,6 +56,21 @@ def start_screen():  # Стартовое окно
                         pass
                     elif 440 <= event.pos[1] <= 480:
                         terminate()
+
+        cycle_iterations += 1
+        if cycle_iterations == 10:  # Кадр логотипа и цвет декораций меняется на каждой 5-ой итерации цикла
+            cycle_iterations = 0
+            tetris_logo_animation(screen, logo_frame)
+            deco_text = deco_font.render('- × - × - × - × - × - × - × - × - × -', True, DECO_TEXT_COLOR[deco_frame])
+            screen.blit(deco_text, (12, 10))
+            screen.blit(deco_text, (12, 580))
+
+            deco_frame += 1
+            if deco_frame > 2:
+                deco_frame = 0
+            logo_frame += 1
+            if logo_frame > 15:
+                logo_frame = 1
 
         pg.display.flip()
         clock.tick(FPS)
