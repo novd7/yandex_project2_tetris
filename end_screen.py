3529316fbb8c24fb33b930a35ec7002e9f60d560
Никита <manticore1305@gmail.com>
2023-01-08 19:51:58 +0300

    endscreen (TODO: finish new record showing) and all another animations

---
+++ end_screen.py
@@ -0,0 +1,114 @@
+import sys
+import pygame as pg
+
+from constants import FPS, SIZE, WIDTH, HEIGHT, BACK_GROUND_COLOR, END_SCREEN_BUTTONS, BUTTON_COLOR, \
+    BUTTON_TARGETED_COLOR, DECO_TEXT_COLOR, TEXT_SCORE_COLOR, TEXT_SCORE_SIGN_COLOR, NEW_RECORD_COLOR
+from draw_button import draw_button
+from logo_animation import logo_animation
+from main import main
+from start_screen import start_screen
+from terminate import terminate
+
+
+def end_screen(score, max_score):
+    '''Конечное окно'''
+    pg.init()
+    pg.display.set_caption('Тетрис - Игра окончена')
+    screen = pg.display.set_mode(SIZE)
+    clock = pg.time.Clock()
+
+    cycle_iterations = 0  # Подсчёты итераций цикла (для работы анимаций)
+    logo_frame = 1  # Номер кадра логотипа
+
+    score_sign_font = pg.font.Font(None, 30)  # Шрифт надписи "Счёт:"
+    score_sign = score_sign_font.render('Счёт:', True, TEXT_SCORE_SIGN_COLOR)
+    screen.blit(score_sign, (220, 320))
+
+    if len(str(score)) == 1:  # Положение счётчика очков
+        score_pos = (235, 350)
+    elif len(str(score)) == 2:
+        score_pos = (225, 350)
+    elif len(str(score)) == 3:
+        score_pos = (215, 350)
+    else:
+        score_pos = (205, 350)
+    score_num_font = pg.font.Font(None, 50)  # Шрифт cчётчика
+    score_num = score_num_font.render(str(score), True, TEXT_SCORE_COLOR)
+    screen.blit(score_num, score_pos)
+
+    new_record_font = pg.font.Font(None, 50)  # Шрифт надписи "Новый рекорд!"
+    new_record_color = 0
+
+    deco_frame = 0
+    deco_color = 0
+    deco_font = pg.font.Font(None, 50)  # Декоративный текст сверху и снизу экрана
+
+    button_font = pg.font.Font(None, 35)  # Шрифт текста на кнопках
+
+    for button in END_SCREEN_BUTTONS:  # Отрисовка кнопок
+        draw_button(screen, button, BUTTON_COLOR, button_font)
+
+    while True:
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+                terminate()
+
+            if event.type == pg.MOUSEMOTION:  # Выделение цветом кнопки, на которую наведён курсор
+                if 80 <= event.pos[0] <= 280 and 520 <= event.pos[1] <= 560:  # Проверки позиции курсора
+                    draw_button(screen, END_SCREEN_BUTTONS[0], BUTTON_TARGETED_COLOR, button_font)
+                elif 300 <= event.pos[0] <= 500 and 520 <= event.pos[1] <= 560:
+                    draw_button(screen, END_SCREEN_BUTTONS[1], BUTTON_TARGETED_COLOR, button_font)
+                else:
+                    for button in END_SCREEN_BUTTONS:
+                        draw_button(screen, button, BUTTON_COLOR, button_font)
+
+            if event.type == pg.MOUSEBUTTONDOWN:  # Действия, вызываемые нажатиями на кнопки
+                if 520 <= event.pos[1] <= 560:  # Проверка позиции курсора по оси x
+                    if 30 <= event.pos[0] <= 280:  # Проверки позиций курсора по оси y
+                        main()
+                    elif 250 <= event.pos[0] <= 500:
+                        start_screen()
+
+        cycle_iterations += 1
+        if cycle_iterations == 10:  # Кадр логотипа, цвет и положение декораций меняется на каждой 10-ой итерации цикла
+            cycle_iterations = 0
+            logo_animation(screen, logo_frame, 'game_over')
+
+            if deco_frame == 0:
+                deco_text = deco_font.render('× - ' * 9 + '×', True, BACK_GROUND_COLOR)
+                screen.blit(deco_text, (12, 10))
+                screen.blit(deco_text, (12, 580))
+                deco_text = deco_font.render('- × ' * 9 + '-', True, DECO_TEXT_COLOR[deco_color])
+                deco_frame = 1
+            else:
+                deco_text = deco_font.render('- × ' * 9 + '-', True, BACK_GROUND_COLOR)
+                screen.blit(deco_text, (12, 10))
+                screen.blit(deco_text, (12, 580))
+                deco_text = deco_font.render('× - ' * 9 + '×', True, DECO_TEXT_COLOR[deco_color])
+                deco_frame = 0
+            screen.blit(deco_text, (12, 10))
+            screen.blit(deco_text, (12, 580))
+
+            if score > max_score:  # TODO: Comparison with max score
+                if new_record_color == 0:
+                    new_record = new_record_font.render('Новый рекорд!', True, BACK_GROUND_COLOR)
+                    screen.blit(new_record, (110, 420))
+                    new_record = new_record_font.render('Новый рекорд!', True, NEW_RECORD_COLOR[new_record_color])
+                    new_record_color = 1
+                else:
+                    new_record = new_record_font.render('Новый рекорд!', True, BACK_GROUND_COLOR)
+                    screen.blit(new_record, (110, 420))
+                    new_record = new_record_font.render('Новый рекорд!', True, NEW_RECORD_COLOR[new_record_color])
+                    new_record_color = 0
+                screen.blit(new_record, (110, 420))
+
+            deco_color += 1
+            if deco_color > 2:
+                deco_color = 0
+            logo_frame += 1
+            if logo_frame > 15:
+                logo_frame = 1
+
+        pg.display.flip()
+        clock.tick(FPS)
--- statistics_screen.py
+++ statistics_screen.py
@@ -1,51 +1,86 @@
 import sys
-
-import pygame
 import pygame as pg
 
-from constants import FPS, SIZE, TETRIS_LOGO_SIZE, START_SCREEN_BUTTONS, BUTTON_COLOR, BUTTON_TARGETED_COLOR, \
-    DECO_TEXT_COLOR
+from constants import FPS, BACK_GROUND_COLOR, STATISTICS_SCREEN_BUTTON, BUTTON_COLOR, BUTTON_TARGETED_COLOR, \
+    DECO_TEXT_COLOR, TOP_10_SIGN_COLOR
+from draw_button import draw_button
 from database import get_max_score_by_name
-
-
-def terminate():  # Прерывание игры (через pg.QUIT или кнопкой "Выйти")
-    pg.quit()
-    sys.exit()
+from terminate import terminate
 
 
 def statistics_screen(screen):
-    def draw_line_of_text(text: str, color: tuple, x, y):
-        font = pg.font.Font(None, 30)
+    '''Экран статистики'''
+    def draw_line_of_text(text: str, color: tuple, x, y, font_size=40):
+        font = pg.font.Font(None, font_size)
         text = font.render(text, True, color)
         screen.blit(text, (x, y))
     screen.fill((0, 0, 0))
     pg.init()
-    pg.display.set_caption('Статистика')
+    pg.display.set_caption('Тетрис - Статистика')
     clock = pg.time.Clock()
+
+    button_font = pg.font.Font(None, 40)  # Шрифт текста на кнопке
+    draw_button(screen, STATISTICS_SCREEN_BUTTON, BUTTON_COLOR, button_font)
+
+    cycle_iterations = 0  # Подсчёт итераций цикла (для работы анимаций)
+    deco_frame = 0
+    deco_color = 0
+    deco_font = pg.font.Font(None, 50)  # Декоративный текст сверху и снизу экрана
+
+    draw_line_of_text("ТОП-10 игр по результату:", TOP_10_SIGN_COLOR, 20, 70, 50)
+    data = get_max_score_by_name()
+    for i in range(10):
+        if len(data) == 0:
+            break
+        else:
+            if i == 0:
+                col = (255, 215, 0)
+            elif i == 1:
+                col = (192, 192, 192)
+            elif i == 2:
+                col = (205, 127, 50)
+            else:
+                col = (255, 255, 255)
+            m = max(data, key=lambda x: x[1])
+            data.remove(m)
+            draw_line_of_text(f"{i + 1}. {m[0]} - {m[1]}", col, 20, 90 + (i + 1) * 30)
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                screen.fill((0, 0, 0))
-                return
-            
-            draw_line_of_text("ТОП 10 игр по результату:", (255, 255, 255), 100, 10)
-            data = get_max_score_by_name()
-            for i in range(10):
-                if len(data) == 0:
-                    break
+                terminate()
+            if event.type == pg.MOUSEMOTION:
+                if 125 <= event.pos[0] <= 350 and 520 <= event.pos[1] <= 560:
+                    draw_button(screen, STATISTICS_SCREEN_BUTTON, BUTTON_TARGETED_COLOR, button_font)
                 else:
-                    if i == 0:
-                        col = (255, 215, 0)
-                    elif i == 1:
-                        col = (192, 192, 192)
-                    elif i == 2:
-                        col = (205, 127, 50)
-                    else:
-                        col = (255, 255, 255)
-                    m = max(data, key=lambda x: x[1])
-                    data.remove(m)
-                    draw_line_of_text(f"{i + 1}. {m[0]} - {m[1]}", col, 10, 10 + (i + 1) * 20)
-                
+                    draw_button(screen, STATISTICS_SCREEN_BUTTON, BUTTON_COLOR, button_font)
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if 125 <= event.pos[0] <= 350 and 520 <= event.pos[1] <= 560:
+                    from start_screen import start_screen
+                    start_screen()
+
+        cycle_iterations += 1
+        if cycle_iterations == 10:  # Кадр логотипа и цвет декораций меняется на каждой 10-ой итерации цикла
+            cycle_iterations = 0
+
+            if deco_frame == 0:
+                deco_text = deco_font.render('× - ' * 9 + '×', True, BACK_GROUND_COLOR)
+                screen.blit(deco_text, (12, 10))
+                screen.blit(deco_text, (12, 580))
+                deco_text = deco_font.render('- × ' * 9 + '-', True, DECO_TEXT_COLOR[deco_color])
+                deco_frame = 1
+            else:
+                deco_text = deco_font.render('- × ' * 9 + '-', True, BACK_GROUND_COLOR)
+                screen.blit(deco_text, (12, 10))
+                screen.blit(deco_text, (12, 580))
+                deco_text = deco_font.render('× - ' * 9 + '×', True, DECO_TEXT_COLOR[deco_color])
+                deco_frame = 0
+            screen.blit(deco_text, (12, 10))
+            screen.blit(deco_text, (12, 580))
+
+            deco_color += 1
+            if deco_color > 2:
+                deco_color = 0
+
         pg.display.flip()
         clock.tick(FPS)
